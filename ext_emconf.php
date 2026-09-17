c58c1b834e89a176cc615fcdffdd05df2f742fe7
Christian Hackl <hackl.chris@googlemail.com>
2026-09-17 13:27:36 +0200

    [TASK] version number bump

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF['hh_ext_filesize'] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.0.0',
+    'version' => '3.1.0',
     'constraints' => [
         'depends' => [
             'php' => '>=8',
