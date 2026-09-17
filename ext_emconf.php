fc62b24d44dc10609ebc67b554e147d8ad87cdd4
Christian Hackl <hackl.chris@googlemail.com>
2026-09-17 13:35:08 +0200

    [FIX] composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF['hh_ext_filesize'] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.1.0',
+    'version' => '3.1.1',
     'constraints' => [
         'depends' => [
             'php' => '>=8',
